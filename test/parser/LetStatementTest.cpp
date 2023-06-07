e97ff6d437b4d83bbb228d066d41f2d39b2beff1
Ezike Ebuka <yerimyah1@gmail.com>
2023-06-07 22:22:21 +0100

    Add: Extra test to the let statement

--- test/parser/LetStatementTest.cpp
+++ test/parser/LetStatementTest.cpp
@@ -4,16 +4,9 @@
 #include "AST.h"
 #include "Parser.h"
 
-SCENARIO("Testing parsing let statement", "[Parser]") {
-    GIVEN("HI") {
-
-    }
-}
-
 using namespace AST;
 
 void letStatementTest(const AST::Statement &statement, std::string_view name) {
-    REQUIRE(statement.tokenLiteral() == TokenType::Let);
 
     const auto *dynCast = dynamic_cast<const LetStatement *>(&statement);
     REQUIRE(dynCast != nullptr);
@@ -22,18 +15,28 @@ void letStatementTest(const AST::Statement &statement, std::string_view name) {
 }
 
 TEST_CASE("Let Statement", "[Parser]") {
-    const std::string input = R"(let x = 5;
+    SECTION("With Correct Input") {
+        const std::string input = R"(let x = 5;
         let y = 10;
         let foobar = 838383;)";
 
-    auto parser = Parser(Lexer(input));
-    auto program = parser.parseProgram();
+        auto parser = Parser(Lexer(input));
+        auto program = parser.parseProgram();
 
-    constexpr std::size_t noOfExpectedStatements = 3;
-    REQUIRE(program.statements.size() == noOfExpectedStatements);
+        constexpr std::size_t noOfExpectedStatements = 3;
+        REQUIRE(program.statements.size() == noOfExpectedStatements);
 
-    constexpr std::string_view expected[noOfExpectedStatements] = {"x", "y", "foobar"};
-    for (std::size_t i = 0; i < noOfExpectedStatements; ++i) {
-        letStatementTest(*program.statements[i].get(), expected[i]);
+        constexpr std::string_view expected[noOfExpectedStatements] = {"x", "y", "foobar"};
+        for (std::size_t i = 0; i < noOfExpectedStatements; ++i) {
+            letStatementTest(*program.statements[i].get(), expected[i]);
+        }
+    }
+
+    SECTION("With Wrong Input") {
+        const std::string input = R"(let x  5;;)";
+
+        auto parser = Parser(Lexer(input));
+        CHECK_THROWS_AS(parser.parseProgram(), ParsingException);
     }
 }
+
